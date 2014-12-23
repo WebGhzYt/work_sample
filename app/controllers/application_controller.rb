d9c693cbbabfdc36ee9261fc8060891a06e2a510
Rohit <godganeshrohit@gmail.com>
2014-12-23 11:23:26 -0800

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-	render text: "<p><h1>Sample application work</h1></p>"
+	render text: "<p><h1>application hello work Page!</h1></p>"
   end
+
 end
